af0ed33fa4508f9f9d3499c291273dda83634672
Konstantin Salyaev <r.cf2003@yandex.ru>
2024-03-12 17:49:28 +0300

    feat: add actors and films entities

---
+++ iternal/entities/actors.go
@@ -0,0 +1,11 @@
+package entities
+
+import "time"
+
+type Actors struct {
+	Id          int
+	FirstName   string
+	LastName    string
+	Gender      string
+	DateOfBirth time.Time
+}
---
+++ iternal/entities/films.go
@@ -0,0 +1,9 @@
+package entities
+
+type Films struct {
+	Id          int
+	Name        string
+	Description string
+	Rating      float64
+	Actors      []Actors
+}
