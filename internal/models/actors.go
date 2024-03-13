a5e144ea07644acef483a9ad4efcb5e9089f035a
Konstantin Salyaev <r.cf2003@yandex.ru>
2024-03-13 12:11:09 +0300

    feat: add actor model

---
+++ internal/models/actors.go
@@ -0,0 +1,11 @@
+package models
+
+import "time"
+
+type Actor struct {
+	Id          int       `json:"id,omitempty"`
+	FirstName   string    `json:"first_name,omitempty"`
+	LastName    string    `json:"last_name,omitempty"`
+	Gender      string    `json:"gender,omitempty"`
+	DateOfBirth time.Time `json:"date_of_birth,omitempty"`
+}
