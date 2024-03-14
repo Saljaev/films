4d9ae5b99c65058dac030d95f0f8a6e1bd53b0fd
Konstantin Salyaev <r.cf2003@yandex.ru>
2024-03-14 14:07:48 +0300

    feat: add middleware for roles

--- internal/handlers/users/role_check.go
+++ internal/handlers/users/role_check.go
@@ -1,52 +1,40 @@
 package userhandler
 
 import (
-	"fmt"
-	"log/slog"
 	"net/http"
-	"tiny/internal/logger/sl"
 )
 
-func (h *UserHandler) CheckRole(log *slog.Logger) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			const op = "UserHandler - Validate"
+func (h *UserHandler) CheckRole(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		const op = "UserHandler - Validate"
 
-			fmt.Println(log)
+		token, err := r.Cookie("Token")
+		claims, err := h.jwt.Parse(token.Value)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("internal error"))
 
-			log := log.With(
-				slog.String("op", op),
-			)
+			return
+		}
 
-			token, err := r.Cookie("Token")
-			claims, err := h.jwt.Parse(token.Value)
-			if err != nil {
-				log.Error("failed to parse access token", sl.Err(err))
+		user, err := h.users.GetById(r.Context(), claims.Id)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			w.Write([]byte("internal error"))
 
-				w.Write([]byte("internal error"))
-				w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
 
-				return
-			}
+		if user.Role == "user" {
+			w.WriteHeader(http.StatusForbidden)
+			w.Write([]byte("not enough rights"))
 
-			user, err := h.users.GetById(r.Context(), claims.Id)
-			if err != nil {
-				log.Error("failed to get user", sl.Err(err))
+			return
+		}
 
-				w.Write([]byte("internal error"))
-				w.WriteHeader(http.StatusInternalServerError)
+		ctx := r.Context()
 
-				return
-			}
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
 
-			_ = user
-			panic("implement me")
-
-			log.Info("user is verified")
-
-			ctx := r.Context()
-
-			next.ServeHTTP(w, r.WithContext(ctx))
-		})
-	}
 }
