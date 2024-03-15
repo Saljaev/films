63b814eabd2031a78d38ba769b1341852f1566d8
Konstantin Salyaev <r.cf2003@yandex.ru>
2024-03-15 17:03:55 +0300

    feat: implement films usecase

--- go.mod
+++ go.mod
@@ -3,7 +3,15 @@ module tiny
 go 1.21.0
 
 require (
-	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
-	golang.org/x/crypto v0.21.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
+	github.com/fatih/color v1.16.0
+	github.com/golang-jwt/jwt/v5 v5.2.1
+	github.com/lib/pq v1.10.9
+	golang.org/x/crypto v0.21.0
+	gopkg.in/yaml.v3 v3.0.1
+)
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 )
---
+++ internal/usecase/films.go
@@ -0,0 +1,136 @@
+package usecase
+
+import (
+	"context"
+	"fmt"
+	"tiny/internal/entities"
+	"tiny/internal/models"
+)
+
+type FilmsUseCase struct {
+	repo FilmsRepo
+}
+
+var _ Films = (*FilmsUseCase)(nil)
+
+func NewFilmsUseCase(repo FilmsRepo) *FilmsUseCase {
+	return &FilmsUseCase{repo}
+}
+
+func (fs *FilmsUseCase) Add(ctx context.Context, f models.Films) (int, error) {
+	const op = "FilmsUseCase - Add"
+
+	actors := []*entities.Actors{}
+	for i := range f.Actors {
+		actors = append(actors, &entities.Actors{
+			FirstName:   f.Actors[i].FirstName,
+			LastName:    f.Actors[i].LastName,
+			Gender:      f.Actors[i].Gender,
+			DateOfBirth: f.Actors[i].DateOfBirth,
+		})
+	}
+
+	newFilm := entities.Films{
+		Name:        f.Name,
+		Description: f.Description,
+		Rating:      f.Rating,
+		ReleaseDate: f.ReleaseDate,
+		Actors:      actors,
+	}
+
+	id, err := fs.repo.Add(ctx, newFilm)
+	if err != nil {
+		return 0, fmt.Errorf("%s - fc.repo.Add: %w", op, err)
+	}
+
+	return id, nil
+}
+
+func (fs *FilmsUseCase) Update(ctx context.Context, f models.Films) error {
+	panic("implement me")
+}
+
+func (fs *FilmsUseCase) Delete(ctx context.Context, id int) error {
+	const op = "FilmsUseCase - Delete"
+
+	err := fs.repo.Delete(ctx, id)
+	if err != nil {
+		return fmt.Errorf("%s - fs.repo.Delete: %w", op, err)
+	}
+
+	return nil
+}
+
+func (fs *FilmsUseCase) SearchByFragment(ctx context.Context, fragment, owner string) ([]*models.Films, error) {
+	const op = "FilmsUseCase - SearchByFragment"
+
+	films, err := fs.repo.SearchByFragment(ctx, fragment, owner)
+	if err != nil {
+		return nil, fmt.Errorf("%s - fs.repo.SearchByFragment: %w", op, err)
+	}
+
+	var res []*models.Films
+
+	for i := range films {
+		var actors []*models.Actor
+
+		for j := range films {
+			actors = append(actors, &models.Actor{
+				FirstName: films[i].Actors[j].FirstName,
+				LastName:  films[i].Actors[j].LastName,
+			})
+		}
+
+		film := models.Films{
+			Name:        films[i].Name,
+			Description: films[i].Description,
+			Rating:      films[i].Rating,
+			Actors:      actors,
+		}
+
+		res = append(res, &film)
+	}
+
+	return res, nil
+}
+
+func (fs *FilmsUseCase) RateByField(ctx context.Context, fragment string, increasing bool) ([]*models.Films, error) {
+	const op = "FilmsUseCase - RateByField"
+
+	var order string
+
+	if increasing {
+		order = "ASC"
+	} else {
+		order = "DESC"
+	}
+
+	films, err := fs.repo.RateByField(ctx, fragment, order)
+	if err != nil {
+		fmt.Errorf("%s - fs.repo.RateByField: %w", op, err)
+	}
+
+	var res []*models.Films
+
+	for i := range films {
+		var actors []*models.Actor
+
+		for j := range films {
+			actors = append(actors, &models.Actor{
+				FirstName: films[i].Actors[j].FirstName,
+				LastName:  films[i].Actors[j].LastName,
+			})
+		}
+
+		film := models.Films{
+			Name:        films[i].Name,
+			Description: films[i].Description,
+			Rating:      films[i].Rating,
+			Actors:      actors,
+		}
+
+		res = append(res, &film)
+	}
+
+	return res, nil
+}
