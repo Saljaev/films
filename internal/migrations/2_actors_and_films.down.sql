74581abec05931e1bb2d517c86f512febc0fdb07
Konstantin Salyaev <r.cf2003@yandex.ru>
2024-03-17 21:58:49 +0300

    fead: add migrations for actors and films

--- internal/migrations/2_actors_and_films.down.sql
+++ internal/migrations/2_actors_and_films.down.sql
@@ -1,5 +1,6 @@
+DROP TABLE IF EXISTS actors_from_films;
+
 DROP TABLE IF EXISTS actors;
 
 DROP TABLE IF EXISTS films;
 
-DROP TABLE IF EXISTS actors_from_films;
--- internal/migrations/2_actors_and_films.up.sql
+++ internal/migrations/2_actors_and_films.up.sql
@@ -6,12 +6,19 @@ CREATE TABLE IF NOT EXISTS actors (
     date_of_birth TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
 
+ALTER TABLE actors ADD CONSTRAINT actor_unique UNIQUE (first_name, last_name, gender, date_of_birth);
+CREATE INDEX first_name_idx ON actors(first_name);
+CREATE INDEX last_name_idx ON actors(last_name);
+
 CREATE TABLE IF NOT EXISTS films (
     id BIGSERIAL PRIMARY KEY NOT NULL,
-    name TEXT NOT NULL,
+    name TEXT UNIQUE NOT NULL,
     description TEXT NOT NULL,
-    rating FLOAT NOT NULL
+    rating FLOAT NOT NULL,
+    release_date TIMESTAMP WITHOUT TIME ZONE NOT NULL
 );
+
+CREATE UNIQUE INDEX film_name_idx ON films(name);
 
 CREATE TABLE IF NOT EXISTS actors_from_films (
     actors_id BIGINT REFERENCES actors(id),
